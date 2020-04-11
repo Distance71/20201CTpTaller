ed1599388508994e9a456133e79285ffcd09d8ca
Agus <agusdaverede@yahoo.com.ar>
2020-04-11 16:22:53 -0300

    Archivos de configuracion

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void initializeProgram(int argc, char* args[]) {
+
+}
+
+int main(int argc, char *args[]) {
+
+    //Call initializeProgram
+
+    // Juego juego
+    // SDL_Renderer *renderer = juego.renderer();
+
+    // juego.loop();
+    // juego.fin();
+
+    cout << "Perro";
+    return 0;
+}
