6bdd900e9e365ccad5f903c08e85155ca03b6610
romeromaxi <mromerov@fi.uba.ar>
2020-06-13 16:41:34 -0300

    Server.h no subido en commit anterior

--- src/server/Server.h
+++ src/server/Server.h
@@ -5,19 +5,23 @@
 #include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "../common/transmitionManager/Socket.h"
 #include "../common/services/Logger.h"
 #include "../common/providers/GameProvider.h"
 
 class Server {
     private:
-    int socket_;
+        Socket *socket_;
+        bool connected_;
 
+        void initializeServer();
+        
     public:
-    Server();
-    ~Server();
+        Server(size_t port);
+        ~Server();
 
-    void initializeServer(size_t port);
-    void run();
+        bool isConnected();
+        int run();
 };
 
 #endif
